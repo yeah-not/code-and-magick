b5f1b3e8657e177310f1eebdc25ff39bcde91781
Alex Yeahnotov <yeahhh.not@gmail.com>
2019-03-16 16:42:24 +0200

    Вывод похожих волшебников, оптимизация кода.

--- js/setup.js
+++ js/setup.js
@@ -24,6 +24,26 @@ var getWizards = function (num) {
   return wizards;
 };
 
+var renderWizard = function (wizard, template) {
+  var wizardElement = template.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var renderWizards = function (wizards, template) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i], template));
+  }
+
+  return fragment;
+};
+
 var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -33,4 +53,10 @@ var WIZARDS_NUM = 4;
 var setupDialog = document.querySelector('.setup');
 setupDialog.classList.remove('hidden');
 
+var similarContainer = document.querySelector('.setup-similar');
+var similarListElement = similarContainer.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
 var wizards = getWizards(WIZARDS_NUM);
+similarListElement.appendChild(renderWizards(wizards, similarWizardTemplate));
+similarContainer.classList.remove('hidden');
