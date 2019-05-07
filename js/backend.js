41ff222b8d81918860d61a17f64fa22e22448025
Alex Yeahnotov <yeahhh.not@gmail.com>
2019-05-07 17:36:47 +0300

    Переработал модуль Backend - вынес общие обработчики загрузки/ошибок

--- js/backend.js
+++ js/backend.js
@@ -8,6 +8,45 @@
   // ---------------
   var URL = 'https://js.dump.academy/code-and-magick/data/';
 
+  var xhrErrorHandler = function (onError) {
+    if (typeof onError === 'function') {
+      onError('Ошибка соединения с сервером');
+    }
+  };
+
+  var xhrTimeoutHandler = function (xhr, onError) {
+    if (typeof onError === 'function') {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + ' мс');
+    }
+  };
+
+  var xhrLoadHandler = function (xhr, onLoad, onError) {
+    var error = '';
+
+    switch (xhr.status) {
+      case 200:
+        if (typeof onLoad === 'function') {
+          onLoad(xhr.response);
+        }
+        break;
+      case 400:
+        error = 'Неверный запрос ' + xhr.status;
+        break;
+      case 401:
+        error = 'Пользователь не авторизован ' + xhr.status;
+        break;
+      case 404:
+        error = 'Ничего не дайдено ' + xhr.status;
+        break;
+      default:
+        error = 'Статус ответа: ' + xhr.status + ' ' + xhr.statusText;
+    }
+
+    if (error && typeof onError === 'function') {
+      onError(error);
+    }
+  };
+
   // Интрефейс
   // ---------------
   window.backend = {
@@ -16,44 +55,9 @@
       xhr.responseType = 'json';
       xhr.timeout = 3000;
 
-      xhr.addEventListener('load', function () {
-        var error = '';
-
-        switch (xhr.status) {
-          case 200:
-            if (typeof onLoad === 'function') {
-              onLoad(xhr.response);
-            }
-            break;
-          case 400:
-            error = 'Неверный запрос ' + xhr.status;
-            break;
-          case 401:
-            error = 'Пользователь не авторизован ' + xhr.status;
-            break;
-          case 404:
-            error = 'Ничего не дайдено ' + xhr.status;
-            break;
-          default:
-            error = 'Статус ответа: ' + xhr.status + ' ' + xhr.statusText;
-        }
-
-        if (error && typeof onError === 'function') {
-          onError(error);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        if (typeof onError === 'function') {
-          onError('Ошибка соединения с сервером');
-        }
-      });
-
-      xhr.addEventListener('timeout', function () {
-        if (typeof onError === 'function') {
-          onError('Запрос не успел выполниться за ' + xhr.timeout + ' мс');
-        }
-      });
+      xhr.addEventListener('load', xhrLoadHandler.bind(this, xhr, onLoad, onError));
+      xhr.addEventListener('error', xhrErrorHandler.bind(this, onError));
+      xhr.addEventListener('timeout', xhrTimeoutHandler.bind(this, xhr, onError));
 
       xhr.open('GET', URL);
       xhr.send();
