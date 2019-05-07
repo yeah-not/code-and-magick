df74028267698ccd2692e99ef6b4d9b0712a1aab
Alex Yeahnotov <yeahhh.not@gmail.com>
2019-05-07 13:18:40 +0300

    Загрузка данных похожих волшебников с выводо в консоль

--- js/backend.js
+++ js/backend.js
@@ -4,16 +4,33 @@
 // ---------------
 
 (function () {
+  // Константы
+  // ---------------
+  var URL = 'https://js.dump.academy/code-and-magick/data';
+
   // Интрефейс
   // ---------------
   window.backend = {
     load: function (onLoad, onError) {
-      if (typeof onLoad === 'function') {
-        onLoad();
-      }
-      if (typeof onError === 'function') {
-        onError();
-      }
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        switch (xhr.status) {
+          case 200:
+            if (typeof onLoad === 'function') {
+              onLoad(xhr.response);
+            }
+            break;
+          default:
+            if (typeof onError === 'function') {
+              onError();
+            }
+        }
+      });
+
+      xhr.open('GET', URL);
+      xhr.send();
     },
     save: function (data, onLoad, onError) {
       if (typeof onLoad === 'function') {
@@ -25,3 +42,11 @@
     },
   };
 })();
+
+// TEMP:
+
+var onLoad = function (wizards) {
+  console.log(wizards);
+};
+
+window.backend.load(onLoad);
