b1d77dcbb7feef00bc72ae5c061a78ced2e6d3c3
Alex Yeahnotov <yeahhh.not@gmail.com>
2019-05-29 10:19:57 +0300

    Module5 2017 - Перетаскивание артефактов (Drag'n'Drop API). Базовая реализация.

---
+++ js/dnd.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// Drag'n'Drop API
+// Магазин артефактов в окне настройки
+// ---------------
+
+(function () {
+  // Обработчики
+  // ---------------
+  var shopDragStartHandler = function (evt) {
+    var target = evt.target;
+
+    if (target.tagName.toLowerCase() !== 'img') {
+      evt.preventDefault();
+      return false;
+    }
+
+    dragItem = target;
+
+    evt.dataTransfer.setData('text/plain', target.alt);
+    artifactsElement.classList.add('drag-in-progress');
+  };
+
+  var artifactsDragOverHanlder = function (evt) {
+    evt.preventDefault();
+    return false;
+  };
+
+  var artifactsDropHandler = function (evt) {
+    if (!dragItem) {
+      evt.preventDefault();
+      return false;
+    }
+
+    var dropItem = dragItem.cloneNode(true);
+    dragItem = null;
+
+    evt.target.appendChild(dropItem);
+    evt.target.classList.remove('drag-over');
+    artifactsElement.classList.remove('drag-in-progress');
+  };
+
+  var artifactsDragEnter = function (evt) {
+    evt.preventDefault();
+
+    if (!dragItem) {
+      return false;
+    }
+
+    evt.target.classList.add('drag-over');
+  };
+
+  var artifactsDragLeave = function (evt) {
+    evt.preventDefault();
+
+    if (!dragItem) {
+      return false;
+    }
+
+    evt.target.classList.remove('drag-over');
+  };
+
+  // Элементы
+  // ---------------
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var artifactsElement = document.querySelector('.setup-artifacts');
+  var dragItem = null;
+
+  // Старт
+  // ---------------
+  shopElement.addEventListener('dragstart', shopDragStartHandler);
+  artifactsElement.addEventListener('dragover', artifactsDragOverHanlder);
+  artifactsElement.addEventListener('drop', artifactsDropHandler);
+  artifactsElement.addEventListener('dragenter', artifactsDragEnter);
+  artifactsElement.addEventListener('dragleave', artifactsDragLeave);
+
+})();
